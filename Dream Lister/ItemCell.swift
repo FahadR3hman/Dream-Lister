ef7dc3e832bce4180f6c4904f772b5a429722f67
Fahad Rehman <Fahad@macs-MacBook-Air.local>
2016-12-13 22:29:56 +0500

    Editing Data

---
+++ Dream Lister/ItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCell.swift
+//  Dream Lister
+//
+//  Created by Fahad Rehman on 12/10/16.
+//  Copyright © 2016 Codecture. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
